4ebe0f9c3da1b8842b408f540f8d2fd7a93b89c6
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2023-12-11 11:03:33 +0100

    Initial integration of new galaxy (bachelor work)

---
+++ client/client_registry.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/fullstorydev/grpcurl"
+	"github.com/jhump/protoreflect/grpcreflect"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ClientRegistry struct {
+	Clients map[string]Client
+}
+
+func (registry *ClientRegistry) NewClient(name, address string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var cc *grpc.ClientConn
+	var err error
+	defer cancel()
+	if cc, err = grpc.DialContext(ctx, address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock()); err != nil {
+		panic(err)
+	}
+	refClient := grpcreflect.NewClientAuto(context.Background(), cc)
+	descSource := grpcurl.DescriptorSourceFromServer(context.Background(), refClient)
+	services, _ := descSource.ListServices()
+	registry.Clients[name] = Client{refClient: refClient, cc: cc, descSource: descSource, grpcServiceName: services[0]}
+}
